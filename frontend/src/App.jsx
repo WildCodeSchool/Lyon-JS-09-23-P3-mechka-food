abfe7b9702439555d86c85a0f9a83ae2a4b876fb
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2023-12-13 14:22:17 +0100

    add US1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import RecipeContainer from "./components/recipesHome/RecipeContainer";
 
 function App() {
   return (
     <div className="App">
       <h1>Mechka Food</h1>
+      <RecipeContainer />
     </div>
   );
 }
