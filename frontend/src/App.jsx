b02e7158985973c5ddd36e4f70f22eb42102ef8f
AXEL LARESE <axelarese@gmail.com>
2023-12-14 11:36:11 +0100

    Carousel CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import RecipesCarrousel from "./components/recipesCarrouselHome/RecipesCarrousel";
 import RecipeContainer from "./components/recipesHome/RecipeContainer";
 
-
 function App() {
   return (
     <div className="App">
