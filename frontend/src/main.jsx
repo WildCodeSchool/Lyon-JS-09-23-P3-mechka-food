eec00447a77882232fcee37d2459b92b292fb7e5
Pat <hoang.patrick@outlook.com>
2023-12-20 12:21:23 +0100

    eslint fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,7 +18,6 @@ const router = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
