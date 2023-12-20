be773a2128ebb337978dee844dbad956ff2078c8
mouram chafikE <mrmouramito@gmail.com>
2023-12-20 15:20:08 +0100

    secend label is deleted

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -12,42 +12,37 @@ export default function Contact() {
             <div className={styles.divInscriptionTitle}>
               <h2 className={styles.inscriptionTitle}>Contactez Nous</h2>
             </div>
-            <label htmlFor="Nom">
-              Nom :
-              <input
-                className={styles.inputContact}
-                type="text"
-                name="text"
-                id="Nom"
-              />
-            </label>
-            <label htmlFor="Prénom">
-              Prénom :
-              <input
-                className={styles.inputContact}
-                type="text"
-                name="text"
-                id="Prénom"
-              />
-            </label>
-            <label htmlFor="Email">
-              Email :
-              <input
-                className={styles.inputContact}
-                type="mail"
-                name="email"
-                id="Email"
-              />
-            </label>
-            <label htmlFor="Message">
-              Message :
-              <input
-                className={styles.Message}
-                type="Message"
-                name="message"
-                id="Message"
-              />
-            </label>
+            <label htmlFor="Nom">Nom :</label>
+            <input
+              className={styles.inputContact}
+              type="text"
+              name="text"
+              id="Nom"
+            />
+
+            <label htmlFor="Prénom">Prénom : </label>
+            <input
+              className={styles.inputContact}
+              type="text"
+              name="text"
+              id="Prénom"
+            />
+
+            <label htmlFor="Email">Email :</label>
+            <input
+              className={styles.inputContact}
+              type="mail"
+              name="email"
+              id="Email"
+            />
+
+            <label htmlFor="Message">Message :</label>
+            <input
+              className={styles.Message}
+              type="Message"
+              name="message"
+              id="Message"
+            />
 
             <button className={styles.subscribeButton} type="button">
               Envoyer
