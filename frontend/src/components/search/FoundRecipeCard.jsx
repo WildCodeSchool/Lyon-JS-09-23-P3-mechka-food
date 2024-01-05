06cd748480f854b61bbb1802e401fc523fd86f1d
AXEL LARESE <axelarese@gmail.com>
2024-01-05 11:00:06 +0100

    Commit suite au commentaire de Matthieu, résolution des conflits effectuée

--- frontend/src/components/search/FoundRecipeCard.jsx
+++ frontend/src/components/search/FoundRecipeCard.jsx
@@ -1,14 +1,11 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 import styles from "./FoundRecipeCard.module.css";
 
-export default function FoundRecipeCard({ image, title, id }) {
+export default function FoundRecipeCard({ image, title }) {
   return (
     <section className={styles.card}>
-      <Link className={styles.card} to={`/recipes/${id}`}>
-        <img src={image} alt={title} className={styles.imageCard} />
-        <h3 className={styles.recipeTitle}>{title}</h3>
-      </Link>
+      <img src={image} alt={title} className={styles.imageCard} />
+      <h3 className={styles.recipeTitle}>{title}</h3>
     </section>
   );
 }
@@ -16,5 +13,4 @@ export default function FoundRecipeCard({ image, title, id }) {
 FoundRecipeCard.propTypes = {
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
 };
