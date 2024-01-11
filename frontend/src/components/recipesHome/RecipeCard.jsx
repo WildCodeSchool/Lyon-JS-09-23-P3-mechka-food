b188a29c7a5baa0b7bebec790f153aae59b9d044
mouram chafikE <mrmouramito@gmail.com>
2024-01-11 20:11:54 +0100

    update card component, add image

--- frontend/src/components/recipesHome/RecipeCard.jsx
+++ frontend/src/components/recipesHome/RecipeCard.jsx
@@ -1,24 +1,27 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import styles from "./RecipeCard.module.css";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import { CardActionArea } from "@mui/material";
 
-export default function RecipeCard({ image, title, id, description }) {
+export default function RecipeCard({ image, id }) {
   return (
-    <section className={styles.card}>
-      <Link to={`/recipes/${id}`}>
-        <img src={image} alt={title} className={styles.imageCard} />
-      </Link>
-      <div className={styles.recipeInfoCard}>
-        <h3 className={styles.recipeTitle}>{title}</h3>
-        <p className={styles.CardDetails}>{description}</p>
-      </div>
-    </section>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <Link to={`/recipes/${id}`}>
+          <CardMedia
+            component="img"
+            height="240"
+            image={image}
+            alt="green iguana"
+          />
+        </Link>
+      </CardActionArea>
+    </Card>
   );
 }
 
 RecipeCard.propTypes = {
   id: PropTypes.number.isRequired,
   image: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
 };
