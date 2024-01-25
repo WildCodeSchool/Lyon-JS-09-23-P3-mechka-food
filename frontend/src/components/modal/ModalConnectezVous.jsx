6a253b37024b73be1c73d03d52ac328a461dc29d
Pat <hoang.patrick@outlook.com>
2024-01-25 14:43:37 +0100

    test ok sur page comment

--- frontend/src/components/modal/ModalConnectezVous.jsx
+++ frontend/src/components/modal/ModalConnectezVous.jsx
@@ -16,14 +16,14 @@ const style = {
   p: 4,
 };
 
-export default function BasicModal() {
+export default function ModalConnectezVous() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   return (
     <div>
-      <Button onClick={handleOpen}>commentaires</Button>
+      <Button onClick={handleOpen}>Connectez-vous</Button>
       <Modal
         open={open}
         onClose={handleClose}
