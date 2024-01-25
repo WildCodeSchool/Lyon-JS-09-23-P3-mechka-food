11f9d7d8a0a9dc585b23e2dc389744845ec37fdc
Pat <hoang.patrick@outlook.com>
2024-01-25 16:33:02 +0100

    modification suite commentaire PR

--- frontend/src/components/comment/Comment.jsx
+++ frontend/src/components/comment/Comment.jsx
@@ -9,7 +9,7 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CommentsByIdRecipe from "./commentsByIdRecipe";
-import ModalConnectezVous from "../modal/ModalConnectezVous";
+import ConnectionVerification from "../modal/ConnectionVerification";
 
 const defaultTheme = createTheme();
 
@@ -121,7 +121,7 @@ export default function Comment() {
               </Button>
             </Box>
           ) : (
-            <ModalConnectezVous />
+            <ConnectionVerification />
           )}
         </Grid>
         <Grid
