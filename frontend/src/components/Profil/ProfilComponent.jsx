05d4961a9d753a52acf1f97ead16a27fbd6fc269
mouram chafikE <mrmouramito@gmail.com>
2024-01-29 11:27:31 +0100

    mis ajour user card

--- frontend/src/components/Profil/ProfilComponent.jsx
+++ frontend/src/components/Profil/ProfilComponent.jsx
@@ -1,15 +1,35 @@
+import React, { useState, useEffect } from "react";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
+import Sheet from "@mui/joy/Sheet";
+import { Container } from "@mui/material";
 import Header from "../Header/Header";
 import Navbar from "../Navbar/Navbar";
 import styles from "./ProfilComponent.module.css";
-import profilPicture from "../../assets/images/profilPicture.png";
 import Sidebar from "../sidebar/Sidebar";
 
 export default function ProfilComponent() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3310/api/user")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  });
+
   return (
     <>
       <Header />
       <Sidebar />
-      <div>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <section className={styles.outline}>
           <img
             className={styles.imageProfil}
@@ -17,27 +37,74 @@ export default function ProfilComponent() {
             alt="Profil"
           />
         </section>
-        <section className={styles.outline}>
-          <div className={styles.mainCardProfil}>
+        <Card
+          orientation="horizontal"
+          sx={{
+            width: "100%",
+            flexWrap: "wrap",
+          }}
+        >
+          <AspectRatio flex ratio="1" maxHeight={182} sx={{ minWidth: 182 }}>
             <img
-              className={styles.svgProfil}
-              src={profilPicture}
-              alt="Profil"
+              src="https://images.pexels.com/photos/8093599/pexels-photo-8093599.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              srcSet="https://images.pexels.com/photos/8093599/pexels-photo-8093599.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2 2x"
+              loading="lazy"
+              alt=""
             />
-            <p>Prénom Nom</p>
-            <p>Genre</p>
-            <p>Email</p>
-          </div>
-        </section>
-        <section className={styles.outline}>
-          <div className={styles.cardInformations}>
-            <p>Pseudo</p>
-            <p>Date de naissance</p>
-            <p>Ville</p>
-            <p>Mot de passe</p>
-          </div>
-        </section>
-      </div>
+          </AspectRatio>
+          {users.map((user) => (
+            <CardContent key={user.id}>
+              <Typography fontSize="xl" fontWeight="lg">
+                Username: {user.username}
+              </Typography>
+              <Typography
+                level="body-sm"
+                fontWeight="lg"
+                textColor="text.tertiary"
+              >
+                Prénom: {user.firstname}
+              </Typography>
+              <Typography
+                level="body-sm"
+                fontWeight="lg"
+                textColor="text.tertiary"
+              >
+                Nom: {user.lastname}
+              </Typography>
+              <Sheet
+                sx={{
+                  bgcolor: "background.level1",
+                  borderRadius: "sm",
+                  p: 1.5,
+                  my: 1.5,
+                  display: "flex",
+                  gap: 2,
+                  "& > div": { flex: 1 },
+                }}
+              >
+                <div>
+                  <Typography level="body-xs" fontWeight="lg">
+                    Recipes
+                  </Typography>
+                  <Typography fontWeight="lg">34</Typography>
+                </div>
+                <div>
+                  <Typography level="body-xs" fontWeight="lg">
+                    Comment
+                  </Typography>
+                  <Typography fontWeight="lg">980</Typography>
+                </div>
+                <div>
+                  <Typography level="body-xs" fontWeight="lg">
+                    Rating
+                  </Typography>
+                  <Typography fontWeight="lg">8.9</Typography>
+                </div>
+              </Sheet>
+            </CardContent>
+          ))}
+        </Card>
+      </Container>
       <Navbar />
     </>
   );
