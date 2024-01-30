f07eef8e08eb4147b98ebabf22aa617b94b2d371
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2024-01-30 11:43:06 +0100

    add button logout if user is connected & add header and sidebar addRecipes page

--- frontend/src/pages/addNewRecipePage/AddRecipePage.jsx
+++ frontend/src/pages/addNewRecipePage/AddRecipePage.jsx
@@ -1,13 +1,17 @@
 import AddRecipeForm from "../../components/addRecipe/AddRecipeForm";
 import ConnectionVerification from "../../components/modal/ConnectionVerification";
 import { useUserContext } from "../../context/userContext";
+import Header from "../../components/Header/Header";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 export default function AddRecipePage() {
   const { userData } = useUserContext();
 
   return (
-    <div>
-      {userData === "null" ? <ConnectionVerification /> : <AddRecipeForm />}
-    </div>
+    <>
+      <Header />
+      <Sidebar />
+      <div>{!userData ? <ConnectionVerification /> : <AddRecipeForm />}</div>
+    </>
   );
 }
