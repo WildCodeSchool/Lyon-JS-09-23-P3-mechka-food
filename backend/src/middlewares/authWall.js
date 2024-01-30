04f383bea61c973c401b1e99e1e0cefb002a6d2f
ElinaTodorova <elina.todorova93@gmail.com>
2024-01-30 14:25:12 +0100

    Cookie expires

--- backend/src/middlewares/authWall.js
+++ backend/src/middlewares/authWall.js
@@ -1,5 +1,7 @@
+// const jwt = require("jsonwebtoken");
+
 const authWall = (req, res, next) => {
-  if (req.cookies) {
+  if (req.cookies.access_token) {
     next();
   } else {
     res.status(401).send("Not Auth");
